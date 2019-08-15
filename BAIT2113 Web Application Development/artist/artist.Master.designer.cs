6e6c8353f0cb88c604d6501d08f4c8a115806072
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2019-08-16 00:27:28 +0800

    Artist.aspx Removed topNav bar, updated sidebar

--- BAIT2113 Web Application Development/artist/artist.Master.designer.cs
+++ BAIT2113 Web Application Development/artist/artist.Master.designer.cs
@@ -31,15 +31,6 @@ namespace BAIT2113_Web_Application_Development.artist {
         protected global::System.Web.UI.WebControls.Label lblAccount;
         
         /// <summary>
-        /// txtGreetings control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label txtGreetings;
-        
-        /// <summary>
         /// contentMain control.
         /// </summary>
         /// <remarks>
--- BAIT2113 Web Application Development/artist/main.aspx
+++ BAIT2113 Web Application Development/artist/main.aspx
@@ -1,8 +1,6 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/artist/artist.Master" AutoEventWireup="true" CodeBehind="main.aspx.cs" Inherits="BAIT2113_Web_Application_Development.artist.main" %>
 <asp:Content ID="Content2" ContentPlaceHolderID="contentMain" runat="server">
-    <a href="artwork/create.aspx">artwork/create.aspx</a>
-    <br />
-    <a href="artwork/view.aspx">artwork/view.aspx</a>
+
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="contentScript" runat="server">
 </asp:Content>
